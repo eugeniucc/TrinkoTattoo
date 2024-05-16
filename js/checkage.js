20f1b620175c523282356741a58f81547fc1e601
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-05-16 20:28:37 +0300

    check 18

--- js/checkage.js
+++ js/checkage.js
@@ -1,1 +1,19 @@
 // сделать проверку возраста если есть 18
+
+"use strict";
+
+function checkAge() {
+  const confirmAge = window.confirm(
+    "Если вам уже исполнилось 18 нажмите Ок. Продолжить?"
+  );
+  if (!confirmAge) {
+    alert(
+      "Вы несовершеннолетний. Скоро будете перенаправлены на другую страницу."
+    );
+    window.location.href = "http://www.google.com";
+  } else {
+    return;
+  }
+}
+
+checkAge();
