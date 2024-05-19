02b2ffd8b6efd09714fcba2297691264c56a6187
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-05-19 22:35:38 +0300

    save

--- js/swiper-portfolio.js
+++ js/swiper-portfolio.js
@@ -1,12 +1,16 @@
-var swiper = new Swiper(".swiper-portfolio", {
-  spaceBetween: 30,
-  effect: "fade",
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+var swiper = new Swiper(".swiper_portfolio", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
   },
 });
