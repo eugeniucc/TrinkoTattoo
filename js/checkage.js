732fec14d8ee06010826e6fd22992832fcefa8a7
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-05-24 14:53:59 +0300

    checkage modal

--- js/checkage.js
+++ js/checkage.js
@@ -1,18 +1,35 @@
-
 "use strict";
 
-function checkAge() {
-  const confirmAge = window.confirm(
-    "Если вам уже исполнилось 18 нажмите Ок. Продолжить?"
-  );
-  if (!confirmAge) {
-    alert(
-      "Вы несовершеннолетний. Скоро будете перенаправлены на другую страницу."
-    );
-    window.location.href = "http://www.google.com";
-  } else {
-    return;
-  }
+const checkAgeForm = document.querySelector(".checkage");
+const checkAgeOverlay = document.querySelector(".checkage_overlay");
+
+const confirm = document.querySelector(".checkage_wrapper-btn--confirm");
+const cancel = document.querySelector(".checkage_wrapper-btn--cancel");
+
+// показ окна
+
+function showCheckAgeForm() {
+  checkAgeForm.classList.add("checkage_show");
+  checkAgeOverlay.classList.add("checkage_show");
 }
 
-checkAge();
+setTimeout(showCheckAgeForm, 1000);
+
+// подтверждение возраста
+
+function checkAgeConfirm() {
+  checkAgeForm.classList.remove("checkage_show");
+  checkAgeOverlay.classList.remove("checkage_show");
+}
+confirm.addEventListener("click", checkAgeConfirm);
+
+// меньше 18
+
+function checkAgeCancel() {
+  alert(
+    "Вы несовершеннолетний. Скоро будете перенаправлены на другую страницу."
+  );
+  window.location.href = "http://www.google.com";
+}
+
+cancel.addEventListener("click", checkAgeCancel);
