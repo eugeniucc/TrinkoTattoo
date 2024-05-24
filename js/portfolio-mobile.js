492ef192a6dc3c06f10befae3b9851221de6ca24
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-05-24 09:00:47 +0300

    porfolio nav media done

--- js/portfolio-mobile.js
+++ js/portfolio-mobile.js
@@ -2,4 +2,8 @@ var swiper = new Swiper(".swiper_portfolio-mobile", {
   pagination: {
     el: ".swiper-pagination",
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
