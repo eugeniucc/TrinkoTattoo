92216db1cbf5f3aa35117ce141f627167fc292f6
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-05-24 20:15:20 +0300

    save

--- js/promo.js
+++ js/promo.js
@@ -1,11 +1,17 @@
 const promo = document.querySelector(".promo");
 const showPromo = document.querySelector(".header_nav-menu--span-red");
+const promoOverlay = document.querySelector(".promo_overlay");
 
 const promoBtnClose = document.querySelector(".promo_wrapper-btn--close");
 
 showPromo.addEventListener("click", () => {
   promo.classList.toggle("promo_show");
+  promoOverlay.classList.toggle("promo_show");
 });
 promoBtnClose.addEventListener("click", () => {
   promo.classList.remove("promo_show");
+});
+promoOverlay.addEventListener("click", () => {
+  promo.classList.remove("promo_show");
+  promoOverlay.classList.remove("promo_show");
 });
