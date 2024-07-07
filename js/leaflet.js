001042a93a78a5b8fcfc5f692636cf7d0e59f182
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-07-08 00:09:16 +0300

    save

---
+++ js/leaflet.js
@@ -0,0 +1,11 @@
+var map = L.map("map").setView([47.0146631, 28.8558081], 16);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([47.0146631, 28.8558081])
+  .addTo(map)
+  .bindPopup("Тату салон TrinkoTattoo в Кишиневе")
+  .openPopup();
