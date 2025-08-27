d072c1d81f0e01187a264ec525807dde13bb582a
eugeniucc <eugeniucoloteniucc@gmail.com>
2025-08-27 18:14:36 +0300

    added new styles

--- js/leaflet-ro.js
+++ js/leaflet-ro.js
@@ -1,11 +1,33 @@
-var map = L.map("map").setView([47.0146631, 28.8558081], 16);
+var map = L.map("map", {
+  zoomControl: true,
+  scrollWheelZoom: false,
+}).setView([47.0146631, 28.8558081], 16);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  subdomains: ["a", "b", "c"],
+  maxZoom: 19,
 }).addTo(map);
 
-L.marker([47.0146631, 28.8558081])
+var customIcon = L.icon({
+  iconUrl: "./icons/icon.png",
+  iconSize: [48, 48],
+  iconAnchor: [24, 48],
+  popupAnchor: [0, -48],
+});
+
+L.marker([47.0146631, 28.8558081], { icon: customIcon })
   .addTo(map)
-  .bindPopup("Salonul de tatuaje - TrinkoTattoo în Chișinău")
+  .bindPopup(
+    `<div style="text-align:center; font-family:Montserrat, sans-serif; max-width:220px;">
+       <h3 style="margin:0; font-size:16px; color:#f24342;">TrinkoTattoo</h3>
+       <p style="margin:4px 0; font-size:14px;">CC Atrium, str. Albișoara 4, Chișinău</p>
+       <a href="https://www.google.com/maps/dir/46.9958656,28.8456704/''/@46.995836,28.7632699,12z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x40c97dfa3f2aeca3:0x506cbca590704cdd!2m2!1d28.8557773!2d47.0147268?entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D" 
+          target="_blank" 
+          style="display:inline-block; margin-top:6px; background:#f24342; color:#fff; padding:6px 12px; border-radius:4px; font-size:14px; text-decoration:none; font-weight:600;">
+         Pornește ruta
+       </a>
+     </div>`
+  )
   .openPopup();
