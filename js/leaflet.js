d96e00ba7a1bfc0e28e6848c18f0e4e8f7318f52
eugeniucc <eugeniucoloteniucc@gmail.com>
2025-08-30 13:03:28 +0300

    sync

--- js/leaflet.js
+++ js/leaflet.js
@@ -11,7 +11,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 var customIcon = L.icon({
-  iconUrl: "./icons/icon.png",
+  iconUrl: "./logo.png",
   iconSize: [48, 48],
   iconAnchor: [24, 48],
   popupAnchor: [0, -48],
@@ -23,7 +23,7 @@ L.marker([47.0146631, 28.8558081], { icon: customIcon })
     `<div style="text-align:center; font-family:Montserrat, sans-serif; max-width:220px;">
        <h3 style="margin:0; font-size:16px; color:#f24342;">TrinkoTattoo</h3>
        <p style="margin:4px 0; font-size:14px;">ТЦ Атриум, ул. Албишоара 4, Кишинев</p>
-       <a href="https://www.google.com/maps/dir/46.9958656,28.8456704/''/@46.995836,28.7632699,12z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x40c97dfa3f2aeca3:0x506cbca590704cdd!2m2!1d28.8557773!2d47.0147268?entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D" 
+       <a href="https://www.google.com/maps/place/TrinkoTattoo/@47.0147268,28.8557773,17z/data=!4m14!1m7!3m6!1s0x40c97dfa3f2aeca3:0x506cbca590704cdd!2sTrinkoTattoo!8m2!3d47.0147268!4d28.8557773!16s%2Fg%2F11y98yqlk9!3m5!1s0x40c97dfa3f2aeca3:0x506cbca590704cdd!8m2!3d47.0147268!4d28.8557773!16s%2Fg%2F11y98yqlk9?entry=ttu&g_ep=EgoyMDI1MDgyNS4wIKXMDSoASAFQAw%3D%3D" 
           target="_blank" 
           style="display:inline-block; margin-top:6px; background:#f24342; color:#fff; padding:6px 12px; border-radius:4px; font-size:14px; text-decoration:none; font-weight:600;">
          Проложить маршрут
